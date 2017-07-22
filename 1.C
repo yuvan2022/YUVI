e06776d581ba2bdd87113346f0b65bae1aac0380
yuvan2022 <30342540+yuvan2022@users.noreply.github.com>
2017-07-22 17:01:39 +0530

    Update 1.C

--- 1.C
+++ 1.C
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("number:");
+  scanf("%d",&n);
+  if( n > 0 )
+  {
+    printf("number is positive");
+  }
+  else if( n < 0)
+  {
+    printf("number is negative");
+  }
+  else
+  {
+    printf ("number is zero");
+  }
+  return 0;
+}
